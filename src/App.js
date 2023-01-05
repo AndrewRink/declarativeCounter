2d4715e9dc0cd734889d89922b76cb2ade8c5cb1
Andrew <andrewcrink@gmail.com>
2023-01-04 21:36:24 -0500

    finished activity in class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [name, setName] = useState('')
+
+  const increment = () => {
+    setCounter(prevState => prevState + 1)
+  }
+  const decrement = () => {
+    if (counter === 0) return
+    setCounter(currentVal => currentVal - 1)
+  }
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Counter:{counter}</h2>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+      
+      <input onChange={handleChange} />
+      <h3>Name: {name} </h3>
     </div>
   );
 }
